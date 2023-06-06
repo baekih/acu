eb06ad2527c4e0aca3191222c95eeaec22451397
ihbaek <ihbaek@ecotro.co.kr>
2023-06-07 08:37:50 +0900

    [ADD] 주변장치 검증.
    1. LCD 타이밍 640x480으로 재설정중

--- Core/Src/stm32f7xx_hal_msp.c
+++ Core/Src/stm32f7xx_hal_msp.c
@@ -348,7 +348,7 @@ void HAL_LTDC_MspInit(LTDC_HandleTypeDef* hltdc)
   /** Initializes the peripherals clock
   */
     PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_LTDC;
-    PeriphClkInitStruct.PLLSAI.PLLSAIN = 243;
+    PeriphClkInitStruct.PLLSAI.PLLSAIN = 202;
     PeriphClkInitStruct.PLLSAI.PLLSAIR = 5;
     PeriphClkInitStruct.PLLSAI.PLLSAIQ = 2;
     PeriphClkInitStruct.PLLSAI.PLLSAIP = RCC_PLLSAIP_DIV2;
