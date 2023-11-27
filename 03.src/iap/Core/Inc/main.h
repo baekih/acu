8cf0ec40c8569362399b97f5da4743c5685d5ba0
ihbaek <ihbaek@ecotro.co.kr>
2023-11-27 17:01:32 +0900

    [BLD] IAP barebone 코드.

---
+++ 03.src/iap/Core/Inc/main.h
@@ -0,0 +1,123 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f7xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define BUZZER_ON_Pin GPIO_PIN_3
+#define BUZZER_ON_GPIO_Port GPIOE
+#define LED_ON_Pin GPIO_PIN_2
+#define LED_ON_GPIO_Port GPIOE
+#define MCU_LCD_SCL_Pin GPIO_PIN_8
+#define MCU_LCD_SCL_GPIO_Port GPIOB
+#define MCU_KEY4_Pin GPIO_PIN_12
+#define MCU_KEY4_GPIO_Port GPIOC
+#define MCU_KEY4_EXTI_IRQn EXTI15_10_IRQn
+#define WDI_Pin GPIO_PIN_15
+#define WDI_GPIO_Port GPIOA
+#define MCU_LCD_SDA_Pin GPIO_PIN_9
+#define MCU_LCD_SDA_GPIO_Port GPIOB
+#define LED_PWM_Pin GPIO_PIN_7
+#define LED_PWM_GPIO_Port GPIOB
+#define MCU_KEY3_Pin GPIO_PIN_11
+#define MCU_KEY3_GPIO_Port GPIOC
+#define MCU_KEY3_EXTI_IRQn EXTI15_10_IRQn
+#define MCU_KEY2_Pin GPIO_PIN_10
+#define MCU_KEY2_GPIO_Port GPIOC
+#define MCU_KEY2_EXTI_IRQn EXTI15_10_IRQn
+#define MCU_PWR_SW_Pin GPIO_PIN_13
+#define MCU_PWR_SW_GPIO_Port GPIOC
+#define MCU_PWR_SW_EXTI_IRQn EXTI15_10_IRQn
+#define MCU_CAN_STB_Pin GPIO_PIN_10
+#define MCU_CAN_STB_GPIO_Port GPIOA
+#define LCD_LR_Pin GPIO_PIN_9
+#define LCD_LR_GPIO_Port GPIOA
+#define LCD_UD_Pin GPIO_PIN_8
+#define LCD_UD_GPIO_Port GPIOA
+#define MCU_KEY1_Pin GPIO_PIN_8
+#define MCU_KEY1_GPIO_Port GPIOC
+#define MCU_KEY1_EXTI_IRQn EXTI9_5_IRQn
+#define PWR_HOLD_Pin GPIO_PIN_6
+#define PWR_HOLD_GPIO_Port GPIOC
+#define LCD_INT_Pin GPIO_PIN_4
+#define LCD_INT_GPIO_Port GPIOA
+#define LCD_INT_EXTI_IRQn EXTI4_IRQn
+#define LCD_STBY_Pin GPIO_PIN_2
+#define LCD_STBY_GPIO_Port GPIOA
+#define TOUCH_RSTn_Pin GPIO_PIN_5
+#define TOUCH_RSTn_GPIO_Port GPIOA
+#define LCD_RSTn_Pin GPIO_PIN_3
+#define LCD_RSTn_GPIO_Port GPIOA
+#define LCD_BKL_PWM_Pin GPIO_PIN_7
+#define LCD_BKL_PWM_GPIO_Port GPIOA
+#define BUZZER_PWM_Pin GPIO_PIN_0
+#define BUZZER_PWM_GPIO_Port GPIOB
+#define DBG_TX_Pin GPIO_PIN_14
+#define DBG_TX_GPIO_Port GPIOB
+#define DBG_RX_Pin GPIO_PIN_15
+#define DBG_RX_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
