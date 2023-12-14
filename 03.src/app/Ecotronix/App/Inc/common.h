ca425d04f789295296ea54583831cccbbb15d70a
ihbaek <ihbaek@ecotro.co.kr>
2023-12-14 11:59:29 +0900

    [ADD] FI_DIN_LCD4 -> FI_DIN_1_0 명칭변경 외. + 안쓰는 .ld 삭제. + hal header extern 위치 변경.

--- 03.src/app/Ecotronix/App/Inc/common.h
+++ 03.src/app/Ecotronix/App/Inc/common.h
@@ -21,7 +21,7 @@
 #include "stm32f767_fi-din_lcd.h"
 #include "stm32f767_fi-din_sdram.h"
 
-//#define FI_DIN_LCD4
+#define FI_DIN_1_0
 
 #pragma pack(push,1)
 typedef struct __RxProtocol
@@ -39,11 +39,6 @@ typedef struct __TxProtocol
 } TxProtocol ;
 #pragma pack(pop)
 
-extern UART_HandleTypeDef huart1, huart2;
-extern CAN_HandleTypeDef hcan1;
-extern QSPI_HandleTypeDef hqspi;
-extern TIM_HandleTypeDef htim3;
-extern TIM_HandleTypeDef htim14;
 extern osMessageQueueId_t EcoQueueUART1Handle;
 extern osMessageQueueId_t EcoQueueNMEA2KRX1Handle;
 extern osMessageQueueId_t EcoQueueNMEA2KTX1Handle;
