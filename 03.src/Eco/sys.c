1ea0c4620b4aa02d9d57032ba70d4f348edd0f29
ihbaek <ihbaek@ecotro.co.kr>
2024-01-30 07:43:59 +0900

    [CHG] .ld 스크립트 파일 이름 및 파티션 이름 변경.

--- 03.src/Eco/sys.c
+++ 03.src/Eco/sys.c
@@ -408,15 +408,15 @@ void setLCDBL(uint8_t lcd_bl)
 
 void initFlashData(void)
 {
-    app_dat *papp_dat = (app_dat*)USER_DAT_ADDRESS;
+    app_dat *papp_dat = (app_dat*)FLASH_DATA_ADDRESS;
     uint32_t crc32_val = HAL_CRC_Calculate(&hcrc, (uint32_t *)papp_dat, sizeof(app_dat)/sizeof(uint32_t) - 1);
 
     if(papp_dat->crc32 != crc32_val)
     {
         g_app_dat = g_app_dat_def;
         printf("%s() Flash re-init with def val. lcd_bl[%d] bzr_vol[%d]\n",__func__, g_app_dat.lcd_bl, g_app_dat.bzr_vol);
-        doFlashErase(USER_DAT_SECTOR);
-        doFlashWrite(USER_DAT_ADDRESS, (uint32_t*)&g_app_dat, sizeof(app_dat)/sizeof(uint32_t));
+        doFlashErase(FLASH_DATA_ADDRESS);
+        doFlashWrite(FLASH_DATA_ADDRESS, (uint32_t*)&g_app_dat, sizeof(app_dat)/sizeof(uint32_t));
     }
     else
     {
@@ -436,8 +436,8 @@ void updateFlashData(void)
     {
         printf("%s() Flash update lcd_bl[%d] bzr_vol[%d]\n", __func__, g_app_dat.lcd_bl, g_app_dat.bzr_vol);
         g_app_dat.crc32 = crc32_val;
-        doFlashErase(USER_DAT_SECTOR);
-        doFlashWrite(USER_DAT_ADDRESS, (uint32_t*)&g_app_dat, sizeof(app_dat)/sizeof(uint32_t));
+        doFlashErase(FLASH_DATA_ADDRESS);
+        doFlashWrite(FLASH_DATA_ADDRESS, (uint32_t*)&g_app_dat, sizeof(app_dat)/sizeof(uint32_t));
     }
 
     return;
