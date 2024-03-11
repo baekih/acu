8f75329d6ff7d07ef760d42b5d0f59ff7751b901
ihbaek <ihbaek@ecotro.co.kr>
2024-03-11 16:01:06 +0900

    [CHG] 구 NMEA2000에서 신규 NMEA2K로 전환.

--- 03.src/Eco/Inc/sys.h
+++ 03.src/Eco/Inc/sys.h
@@ -240,12 +240,14 @@ bool getKeyPending(uint8_t idx);
 void initTS(void);
 bool getTS(uint16_t*, uint16_t*);
 void InitQSPI(void);
-void CAN1_SendFrame(uint32_t, uint8_t*, uint8_t);
 void setBuzzer(uint8_t);
 void setLCDBL(uint8_t);
 void initFlashData(void);
 void updateFlashData(void);
 void setLCDTestImage(uint8_t);
+#if !defined (ECO_BOOT2)
+void CAN1_SendFrame(uint32_t, uint8_t*, uint8_t);
+#endif
 
 #ifdef __cplusplus
 }
