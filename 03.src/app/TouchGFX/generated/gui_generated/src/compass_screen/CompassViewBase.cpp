6001b7ca256edce8da64f3b96f67819ca44ccf57
ihbaek <ihbaek@ecotro.co.kr>
2025-03-14 14:31:02 +0900

    [BLD] app : CubeIDE 1.18.0 CubeMX 6.14.0 TouchGFX 4.25.0

--- 03.src/app/TouchGFX/generated/gui_generated/src/compass_screen/CompassViewBase.cpp
+++ 03.src/app/TouchGFX/generated/gui_generated/src/compass_screen/CompassViewBase.cpp
@@ -10,7 +10,7 @@
 CompassViewBase::CompassViewBase()
 {
     touchgfx::CanvasWidgetRenderer::setupBuffer(canvasBuffer, CANVAS_BUFFER_SIZE);
-    
+
     __background.setPosition(0, 0, 800, 480);
     __background.setColor(touchgfx::Color::getColorFromRGB(0, 0, 0));
     add(__background);
--- 03.src/app/TouchGFX/generated/gui_generated/src/speed_screen/SpeedViewBase.cpp
+++ 03.src/app/TouchGFX/generated/gui_generated/src/speed_screen/SpeedViewBase.cpp
@@ -10,7 +10,7 @@
 SpeedViewBase::SpeedViewBase()
 {
     touchgfx::CanvasWidgetRenderer::setupBuffer(canvasBuffer, CANVAS_BUFFER_SIZE);
-    
+
     __background.setPosition(0, 0, 800, 480);
     __background.setColor(touchgfx::Color::getColorFromRGB(0, 0, 0));
     add(__background);
