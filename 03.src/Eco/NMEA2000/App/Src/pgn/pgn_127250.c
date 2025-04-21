6e58c46a549c09c5bedb6713d65451d13f990810
ihbaek <ihbaek@ecotro.co.kr>
2025-04-21 11:43:49 +0900

    [CHG] 코드 정리.

--- 03.src/Eco/NMEA2000/App/Src/pgn/pgn_127250.c
+++ 03.src/Eco/NMEA2000/App/Src/pgn/pgn_127250.c
@@ -28,12 +28,12 @@ uint8_t     NMEAReserved;               // 6  bit
 void PGN127250_GetFieldValue(NmeaPgn* pgnId, uint8_t len, uint8_t *buf)
 {
 
-    SequenceID             =  GetBuf_1ByteUInt(len, 0, buf); 	// 8  bits
-    HeadingSensorReading   =  GetBuf_2ByteUInt(len, 1, buf); 	// 16 bits
-    Deviation              =  GetBuf_2ByteUInt(len, 3, buf); 	// 16 bits
-    Variation              =  GetBuf_2ByteUInt(len, 5, buf); 	// 16 bits
-    HeadingSensorReference = (GetBuf_1ByteUInt(len, 7, buf) & 0x03); // 2  bits
-    NMEAReserved           = (GetBuf_1ByteUInt(len, 7, buf) & 0xC0) >> 6;		// 6  bits
+    SequenceID             =  GetBuf_1ByteUInt(len, 0, buf);                    // 8  bits
+    HeadingSensorReading   =  GetBuf_2ByteUInt(len, 1, buf);                    // 16 bits
+    Deviation              =  GetBuf_2ByteUInt(len, 3, buf);                    // 16 bits
+    Variation              =  GetBuf_2ByteUInt(len, 5, buf);                    // 16 bits
+    HeadingSensorReference = (GetBuf_1ByteUInt(len, 7, buf) & 0x03);            // 2  bits
+    NMEAReserved           = (GetBuf_1ByteUInt(len, 7, buf) & 0xC0) >> 6;       // 6  bits
 
 /*  printf("(%d)", pgnId->mSA);
     printf("Receive Vessel Heading - %3d,", SequenceID);
